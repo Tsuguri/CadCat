d8f7698b1debfcda6cb3de8f01d5c83c5c59d67f
Adam Winiarczuk <adamosca@gmail.com>
2017-03-22 23:28:18 +0100

    UserControls and Ray

---
+++ CadCat/Math/Ray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadCat.Math
+{
+	public struct Ray
+	{
+		public Vector3 Origin;
+
+		public Vector3 Direction;
+
+		public Ray(Vector3 origin, Vector3 direction)
+		{
+			this.Origin = origin;
+			this.Direction = direction;
+		}
+
+		public Vector3 GetPoint(double distance)
+		{
+			return Origin + Direction * distance;
+		}
+	}
+}
