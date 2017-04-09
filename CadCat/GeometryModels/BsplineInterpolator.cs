e5683b94b2a31f53adc9ef27763e31101417cdf4
Adam Winiarczuk <adamosca@gmail.com>
2017-04-09 02:23:25 +0200

    Sth is working

--- CadCat/GeometryModels/BsplineInterpolator.cs
+++ CadCat/GeometryModels/BsplineInterpolator.cs
@@ -7,13 +7,16 @@ using System.Threading.Tasks;
 using CadCat.DataStructures;
 using CadCat.Rendering;
 using System.Windows.Input;
+using CadCat.Math;
 
 namespace CadCat.GeometryModels
 {
-	class BsplineInterpolator : PointModel, IChangeablePointCount
+	class BsplineInterpolator : PointModel, IChangeablePointCount, ITypeChangeable
 	{
 		bool changed = false;
 
+
+		private List<Vector3> renderPoints;
 		public BsplineInterpolator(List<CatPoint> points)
 		{
 			changed = false;
@@ -32,6 +35,7 @@ namespace CadCat.GeometryModels
 				return deletePointsCommand ?? (deletePointsCommand = new Utilities.CommandHandler(DeleteSelectedPoints));
 			}
 		}
+
 		private void DeleteSelectedPoints()
 		{
 			var list = points.Where((x) => x.IsSelected).ToList();
@@ -39,11 +43,102 @@ namespace CadCat.GeometryModels
 				RemovePoint(point, true);
 		}
 
+
+		private void CalculateWhatever()
+		{
+			var pts = points.Select(pt => pt.Point.Position).ToList();
+			var distances = new double[pts.Count - 1];
+			var knots = new double[pts.Count];
+
+			double sum = 0;
+			for (int i = 0; i < pts.Count - 1; i++)
+			{
+				distances[i] = (pts[i + 1] - pts[i]).Length();
+				knots[i] = sum;
+				sum += distances[i];
+			}
+			knots[knots.Length - 1] = sum;
+
+			var alphas = new double[pts.Count - 3];
+			var betas = new double[pts.Count - 3];
+			var eras = new Vector3[pts.Count - 2];
+			int j = 0;
+			for (j = 1; j < pts.Count - 2; j++)
+			{
+				alphas[j - 1] = distances[j] / (distances[j] + distances[j + 1]);
+				betas[j - 1] = distances[j] / (distances[j] + distances[j - 1]);
+
+				eras[j - 1] = (((pts[j + 1] - pts[j]) / distances[j]) - ((pts[j] - pts[j - 1]) / distances[j - 1])) * 3 / (distances[j] + distances[j - 1]);
+			}
+
+			eras[j - 1] = (((pts[j + 1] - pts[j]) / distances[j]) - ((pts[j] - pts[j - 1]) / distances[j - 1])) * 3 / (distances[j] + distances[j - 1]); ;
+			var diagonal = new double[pts.Count - 2];
+			for (j = 0; j < diagonal.Length; j++)
+				diagonal[j] = 2;
+
+			var results = Utils.SolveMultidiagonalMatrixEquation(alphas, diagonal, betas, eras);
+
+
+			var a = pts.ToArray();
+			var b = new Vector3[pts.Count - 1];
+			var c = new Vector3[pts.Count];
+			var d = new Vector3[pts.Count - 1];
+
+			results.ToArray().CopyTo(c, 1);
+
+			for (int i = 0; i < d.Length; i++)
+			{
+				var del = c[i + 1] - c[i];
+				var dist = (3 * distances[i]);
+				d[i] = (del) / dist;
+			}
+
+			for (int i = 0; i < b.Length; i++)
+				b[i] = (a[i + 1] - a[i]) / distances[i] - (c[i] + d[i] * distances[i]) * distances[i];
+
+
+			double step = sum / 100;
+			double travelled = 0;
+			int interval = 0;
+			renderPoints = new List<Vector3>();
+			//renderPoints.Add(pts[0]);
+			for (int i = 0; i < 3; i++)
+			{
+				//travelled += step;
+				//while (interval < distances.Length && distances[interval] < travelled)
+				//{
+				//	interval += 1;
+				//	if (interval < distances.Length)
+				//		travelled -= distances[interval];
+				//}
+
+				//if (interval >= distances.Length)
+				//	break;
+				for(int k = 0; k<10; k++)
+				{
+					travelled = k*distances[interval] / 10;
+					var pos = a[interval] + (b[interval] + (c[interval] + d[interval] * travelled) * travelled) * travelled;
+					renderPoints.Add(pos);
+
+				}
+				//travelled = distances[interval];
+				//pos = a[interval] + (b[interval] + (c[interval] + d[interval] * travelled) * travelled) * travelled;
+				//renderPoints.Add(pos);
+
+				interval += 1;
+			}
+			renderPoints.Add(pts[pts.Count - 1]);
+
+			int x = 0;
+
+		}
+
 		public void AddPoint(CatPoint point)
 		{
 			points.Add(new PointWrapper(point));
 			point.OnDeleted += OnPointDelete;
 			point.OnChanged += OnPointChanged;
+			changed = true;
 		}
 
 		private void OnPointDelete(CatPoint point)
@@ -64,6 +159,7 @@ namespace CadCat.GeometryModels
 				point.Point.OnDeleted -= OnPointDelete;
 				point.Point.OnChanged -= OnPointChanged;
 			}
+			changed = true;
 		}
 
 		public void RemovePoint(CatPoint point)
@@ -89,6 +185,17 @@ namespace CadCat.GeometryModels
 		{
 			base.Render(renderer);
 
+			CalculateWhatever();
+			renderer.Points = renderPoints;
+			renderer.UseIndices = false;
+			renderer.Transform();
+			renderer.DrawLines();
+
+		}
+
+		public void ChangeType()
+		{
+			CalculateWhatever();
 		}
 	}
 }
--- CadCat/Math/Utils.cs
+++ CadCat/Math/Utils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace CadCat.Math
 {
@@ -35,6 +36,38 @@ namespace CadCat.Math
 			{
 				return 0.00001;
 			}
+		}
+
+		public static List<Vector3> SolveMultidiagonalMatrixEquation(Real[] underDiagonal, Real[] diagonal, Real[] overDiagonal, Vector3[] results)
+		{
+			Debug.Assert(underDiagonal.Length == diagonal.Length - 1);
+			Debug.Assert(overDiagonal.Length == diagonal.Length - 1);
+			Debug.Assert(results.Length == diagonal.Length);
+
+			var u = new double[diagonal.Length];
+			var l = new double[diagonal.Length];
+			var y = new Vector3[diagonal.Length];
+
+			
+			u[0] = diagonal[0];
+			for (int i = 1; i < diagonal.Length; i++)
+			{
+				l[i]=(underDiagonal[i-1] / u[i-1]);
+				u[i]=(diagonal[i] - l[i] * overDiagonal[i-1]);
+			}
+			y[0] = results[0];
+			for (int i = 1; i < diagonal.Length; i++)
+			{
+				y[i]=results[i] - y[i-1] * l[i];
+			}
+
+			var x = new Vector3[diagonal.Length];
+			x[x.Length - 1] = y[y.Length - 1] / u[u.Length - 1];
+			for(int i=x.Length-2;i>=0;i--)
+			{
+				x[i] = (y[i] -  x[i + 1]*overDiagonal[i]) / u[i];
+			}
+			return x.ToList();
 		}
 	}
 }
